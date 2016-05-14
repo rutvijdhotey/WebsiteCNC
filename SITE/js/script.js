e7d74b3b645e8581694a1ca96f2f8f1238d2af81
rutvijdhotey <rutvij.dhotey@gmail.com/Users/rutvijdhotey/Documents/Sofwares/apache-ant-1.9.6/bin/antRun>
2016-05-13 23:04:31 -0400

    SPA Dynamic loading of HomePage

--- SITE/js/script.js
+++ SITE/js/script.js
@@ -1,11 +1,12 @@
+
+	
 $(function () { // this means that we are making our function effective immediately after we load our HTML before loading all the pics and everything.
 				//Same as document.addEventListener("DOMConetentLoaded")
 	//Same as document.querySelector("#navbarToggle")
-	$("#navbarToggle").blur(function(event) {
+	$("#navbarToggle").blur ( function (event) {
 		var screenWidth = window.innerWidth;
-		if (screenWidth< 768) {
-			$("#collabsable-nav").collapse('hide');
+		if (screenWidth < 768) {
+			$("#collapsable-nav").collapse('hide');
 		}
 	});
 });			
-	
