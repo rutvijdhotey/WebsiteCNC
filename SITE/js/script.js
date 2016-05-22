d72b80222e0c9d020a1b191352791311f979ec75
rutvijdhotey <rutvij.dhotey@gmail.com/Users/rutvijdhotey/Documents/Sofwares/apache-ant-1.9.6/bin/antRun>
2016-05-22 14:06:07 -0400

    adding Javscript functionalities

--- SITE/js/script.js
+++ SITE/js/script.js
@@ -10,3 +10,48 @@ $(function () { // this means that we are making our function effective immediat
 		}
 	});
 });			
+
+
+
+(function (global) {
+
+var dc = {};
+
+var homeHtml = "snippets/home-snippet.html";
+
+// Convenience function for inserting innerHTML for 'select'
+var insertHtml = function (selector, html) {
+  var target = document.querySelector(selector);
+  target.innerHTML = html;
+};
+
+// Show loading icon inside element identified by 'selector'.
+var showLoading = function (selector) {
+  console.log('Reached showLoading');
+  var html = "<div class='text-center'>";
+  html += "<img src='images/ajax-loader.gif'></div>";
+  insertHtml(selector, html);
+};
+
+
+// On page load (before images or CSS)
+document.addEventListener("DOMContentLoaded", function (event) {
+  
+   console.log('Reached after showloading');
+// On first load, show home view
+showLoading("#main-content");
+$ajaxUtils.sendGetRequest(
+  homeHtml, 
+  function (responseText) {
+    document.querySelector("#main-content")
+      .innerHTML = responseText;
+  }, 
+  false);
+});
+
+console.log('Reached end');
+global.$dc = dc;
+
+})(window);
+
+
